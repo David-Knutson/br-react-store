3cac77e028e39a52ca6488585778f1231dfc6249
david-knutson <david.knutson@adeptry.com>
2021-05-16 20:02:54 -0400

    some refactoring in App.tsx and Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 import Layout from "./components/Layout";
 import MainSwitch from "./components/MainSwitch";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <MainSwitch />
-      </Layout>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Layout>
+          <MainSwitch />
+        </Layout>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Navbar, Nav, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 import { MOCKDATA } from "../mockdata/MockData";
 import Cart from "./cart/cart";
 
@@ -10,14 +11,22 @@ const Header: React.FC = () => {
   return (
     <>
       <Navbar className="px-4 shadow-sm" bg="white" expand="lg">
-        <Navbar.Brand>React Store</Navbar.Brand>
+        <Navbar.Brand>
+          <h2>React Store</h2>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             {productData.map((product) => (
-              <Nav.Link href={`/product/?id=${product.id}`} key={product.id}>
+              // supposed to use to instead of href
+              // <Nav.Link href={`/product/?id=${product.id}`} key={product.id}>
+              <NavLink
+                className="mx-4"
+                to={`/product/?id=${product.id}`}
+                key={product.id}
+              >
                 {product.name}
-              </Nav.Link>
+              </NavLink>
             ))}
           </Nav>
         </Navbar.Collapse>
